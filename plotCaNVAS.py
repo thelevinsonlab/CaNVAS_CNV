68bb866f3c98ccc2b09fdbc6d8fb6515ab8bd354
rtlevinson <rtlevinson@users.noreply.github.com>
2022-06-15 15:51:47 +0200

    update LeftRight plotting

--- plotCaNVAS.py
+++ plotCaNVAS.py
@@ -19,23 +19,31 @@ def createPlots(outputpath):
 		print(f)
 		dat=pd.read_csv(outputpath+"/"+f, delim_whitespace=True)
 		x=dat["meanLRRIn"]
-		y1 = (dat["meanLRROutleft"] +dat["meanLRROutright"])/2
-		y2 = dat["BAFHomozygote"]
-		y3 = dat["BAFTrisomic"]
-		y4 = dat["BAFDisomic"]
-		fig=make_subplots(rows=2, cols=2)
+		y1 = dat["meanLRROutleft"]
+		y2 = dat["meanLRROutright"]
+		y3 = (dat["meanLRROutleft"] +dat["meanLRROutright"])/2
+		y4 = dat["BAFHomozygote"]
+		y5 = dat["BAFTrisomic"]
+		y6 = dat["BAFDisomic"]
+		fig=make_subplots(rows=3, cols=2)
 		fig.add_trace(go.Scatter(x=x, y=y1, mode="markers",text=dat.SubjectID,hoverinfo='text'), row=1, col=1)
 		fig.add_trace(go.Scatter(x=x, y=y2, mode="markers",text=dat.SubjectID,hoverinfo='text'), row=1, col=2)
 		fig.add_trace(go.Scatter(x=x, y=y3, mode="markers",text=dat.SubjectID,hoverinfo='text'), row=2, col=1)
 		fig.add_trace(go.Scatter(x=x, y=y4, mode="markers",text=dat.SubjectID,hoverinfo='text'), row=2, col=2)
+		fig.add_trace(go.Scatter(x=x, y=y5, mode="markers",text=dat.SubjectID,hoverinfo='text'), row=3, col=1)
+		fig.add_trace(go.Scatter(x=x, y=y6, mode="markers",text=dat.SubjectID,hoverinfo='text'), row=3, col=2)
 		fig.update_xaxes(title_text="MeanLRRIn", row=1, col=1)
 		fig.update_xaxes(title_text="MeanLRRIn", row=1, col=2)
 		fig.update_xaxes(title_text="MeanLRRIn",  row=2, col=1)
 		fig.update_xaxes(title_text="MeanLRRIn", row=2, col=2)
-		fig.update_yaxes(title_text="AverageLRROut", row=1, col=1)
-		fig.update_yaxes(title_text="BAF Homozygote", row=1, col=2)
-		fig.update_yaxes(title_text="BAF Trisomic", row=2, col=1)
-		fig.update_yaxes(title_text="BAF Disomic", row=2, col=2)
+		fig.update_xaxes(title_text="MeanLRRIn",  row=3, col=1)
+		fig.update_xaxes(title_text="MeanLRRIn", row=3, col=2)
+		fig.update_yaxes(title_text="MeanLRROutLeft", row=1, col=1)
+		fig.update_yaxes(title_text="MeanLRROutRight", row=1, col=2)
+		fig.update_yaxes(title_text="AverageLRROut", row=2, col=1)
+		fig.update_yaxes(title_text="BAF Homozygote", row=2, col=2)
+		fig.update_yaxes(title_text="BAF Trisomic", row=3, col=1)
+		fig.update_yaxes(title_text="BAF Disomic", row=3, col=2)
 		fig.update_layout(height=1000, width=2000, title_text=f)	
 		fig.update_annotations(clicktoshow='onoff')		
 		fig.write_html(outputpath+"/plots/"+f+"plot.html")
